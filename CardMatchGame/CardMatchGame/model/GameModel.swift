9b545b826bb04bf0dea2da8ab40260b243d8e8e5
shruthi.da <shruthi.da1@gmail.com>
2023-01-03 03:00:54 +0800

    Initial Commit

---
+++ CardMatchGame/CardMatchGame/model/GameModel.swift
@@ -0,0 +1,72 @@
+//
+//  GameModel.swift
+//  Memorizee
+//
+//  Created by Shruthi_ML on 4/10/22.
+//
+
+import Foundation
+import SwiftUI
+
+
+//Make CardContentType extend Equatable so it can be compared
+struct GameModel<CardContentType> where CardContentType: Equatable {
+    
+    var lastCardIndex : Int?
+    var cardList : [GameCard]
+    var isGameComplete: Bool = false
+    
+    //Function should be marked as mutating when instance variable is modified
+    mutating func chooseCard(selectedGameCard: GameCard){
+        
+        if let chosenCardIndex = cardList.firstIndex(where: { $0.id == selectedGameCard.id}), selectedGameCard.isFaceUp != true, !selectedGameCard.isMatched {
+            
+            if let lastChosenCardIndex = lastCardIndex {
+                if(cardList[lastChosenCardIndex].cardIcon == cardList[chosenCardIndex].cardIcon) {
+                    cardList[lastChosenCardIndex].isMatched = true
+                    cardList[chosenCardIndex].isMatched = true
+                }
+                lastCardIndex = nil
+            } else {
+                for index in cardList.indices{
+                    cardList[index].isFaceUp = false
+                }
+                lastCardIndex = chosenCardIndex
+            }
+            cardList[chosenCardIndex].isFaceUp = true
+        }
+        
+    }
+    
+    init(noOfCards: Int, cardContent : (Int) -> CardContentType){
+        print("1. Number of cards init - \(noOfCards)")
+        cardList = Array()
+        for i in 0..<noOfCards {
+            cardList.append(GameCard(id: (i*2), cardIcon: cardContent(i)))
+            cardList.append(GameCard(id: (i*2+1), cardIcon: cardContent(i)))
+        }
+        print("card list is updateddddd   1 \(cardList.count)")
+        cardList.shuffle()
+    }
+    
+    init(noOfCards: Int){
+        print("2. Number of cards init - \(noOfCards)")
+        cardList = Array()
+        for i in 0..<noOfCards {
+            cardList.append(GameCard(id: (i*2)))
+            cardList.append(GameCard(id: (i*2+1)))
+        }
+        print("card list is updateddddd  2  \(cardList.count)")
+        cardList.shuffle()
+    }
+    
+    //Identifiable is required to idenentify each card
+    struct GameCard : Identifiable {
+        var id: Int
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var cardIcon: CardContentType?
+    }
+    
+    
+}
---
+++ CardMatchGame/CardMatchGame/model/ThemeColorSet.swift
@@ -0,0 +1,19 @@
+//
+//  ThemeColorSet.swift
+//  Memorizee
+//
+//  Created by Shruthi_ML on 18/12/22.
+//
+
+import Foundation
+
+struct ThemeColorSet: Identifiable, Equatable, Codable {
+    
+    var id: Int
+    var c1: String
+    var c2: String
+    var c3: String
+    var c4: String
+    var c5: String
+    
+}
