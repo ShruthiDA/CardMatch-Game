2f9f2ab4d3f259ea7b9192103778c5008a88b915
shruthi.da <shruthi.da1@gmail.com>
2023-01-03 02:10:14 +0800

    Initial Commit

---
+++ CardMatchGame/CardMatchGame/CardMatchGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardMatchGameApp.swift
+//  CardMatchGame
+//
+//  Created by Shruthi_ML on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CardMatchGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
